5434c08876e417b16fd2e96753bc7ff6a0beddf7
tharathip15 <tharathip15@users.noreply.github.com>
2026-08-04 16:52:28 +0700

    feat: make every route handle point freely 2D draggable

--- app.js
+++ app.js
@@ -3695,7 +3695,7 @@ function drawConnections(renderContext = currentChartRenderContext) {
     });
     if (selectedRouteGeometry) {
         const { routeGeometry, parentId, childId } = selectedRouteGeometry;
-        createConnectionRouteHandle("branch", routeGeometry.branchHandle, parentId, childId);
+        createConnectionRouteHandle("both", routeGeometry.branchHandle, parentId, childId);
         createConnectionRouteHandle("both", routeGeometry.laneHandle, parentId, childId);
     }
     updateConnectionRouteToolbar();
